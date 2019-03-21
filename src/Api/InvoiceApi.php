9c2403672141c4d3f9d933ef98f55de5217a7252
James Weston <james@savannah.io>
2019-03-21 13:16:29 -0400

    Adding invoices/upcoming API

--- src/Api/InvoiceApi.php
+++ src/Api/InvoiceApi.php
@@ -6,6 +6,7 @@ namespace jamesvweston\Stripe\Api;
 use jamesvweston\Stripe\Exceptions\StripeException;
 use jamesvweston\Stripe\Requests\GetInvoicesRequest;
 use jamesvweston\Stripe\Responses\BalanceTransaction;
+use jamesvweston\Stripe\Responses\Invoice;
 use jamesvweston\Stripe\Responses\InvoiceCollection;
 
 class InvoiceApi extends BaseApi
@@ -22,6 +23,19 @@ class InvoiceApi extends BaseApi
         $request['expand[]']     = 'data.charge';
         $data                           = $this->api->makeHttpRequest('get', 'invoices', $request);
         return $this->json_mapper->map($data, new InvoiceCollection());
+    }
+
+    /**
+     * @param   array $request
+     * @return  Invoice
+     * @throws StripeException
+     * @throws \JsonMapper_Exception
+     */
+    public function upcoming ($request = [])
+    {
+        $request                 = ($request instanceof \JsonSerializable) ? $request->jsonSerialize() : $request;
+        $data                           = $this->api->makeHttpRequest('get', 'invoices/upcoming', $request);
+        return $this->json_mapper->map($data, new Invoice());
     }
 
 }
